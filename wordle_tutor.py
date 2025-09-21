8db33c52891363e2fb72c3293ae71caea53d6ebd
Sameer Segal <sameersegal@microsoft.com>
2025-09-21 15:41:28 +0000

    Updated the env

--- wordle_tutor.py
+++ wordle_tutor.py
@@ -56,37 +56,53 @@ def partial_credit_reward_func(parser, completion, answer, **kwargs) -> float:
 
     num_greens = sum(s == "G" for s in letters.values())
     num_yellows = sum(s == "Y" for s in letters.values())
-    total = len(letters)
 
     return 0.2 * num_greens + 0.1 * num_yellows
 
 
-# environment loader
+class WordleTutorEnv(TextArenaEnv):
+
+    def __init__(
+        self,
+        num_train_examples: int = 2000,
+        num_eval_examples: int = 20,
+        use_think: bool = True,
+        **kwargs,
+    ):
+        if use_think:
+            system_prompt = THINK_GUESS_SYSTEM_PROMPT
+            parser = vf.XMLParser(
+                fields=["think", "guess"], answer_field="guess")
+        else:
+            system_prompt = NOTHINK_GUESS_SYSTEM_PROMPT
+            parser = vf.XMLParser(fields=["guess"], answer_field="guess")
+
+        rubric = vf.Rubric(parser=parser)
+        rubric.add_reward_func(check_answer_reward_func)
+        rubric.add_reward_func(partial_credit_reward_func)
+        rubric.add_reward_func(count_turns_reward_func)
+        rubric.add_reward_func(parser.get_format_reward_func(), weight=0.2)
+
+        super().__init__(
+            game="Wordle-v0",
+            num_train_examples=num_train_examples,
+            num_eval_examples=num_eval_examples,
+            system_prompt=system_prompt,
+            parser=parser,
+            rubric=rubric,
+            feedback_fn=wordle_feedback_fn,
+            **kwargs,
+        )
+
+
 def load_environment(
     num_train_examples: int = 2000,
     num_eval_examples: int = 20,
     use_think: bool = True,
 ):
-    if use_think:
-        system_prompt = THINK_GUESS_SYSTEM_PROMPT
-        parser = vf.XMLParser(fields=["think", "guess"], answer_field="guess")
-    else:
-        system_prompt = NOTHINK_GUESS_SYSTEM_PROMPT
-        parser = vf.XMLParser(fields=["guess"], answer_field="guess")
 
-    rubric = vf.Rubric(parser=parser)
-    rubric.add_reward_func(check_answer_reward_func)
-    rubric.add_reward_func(partial_credit_reward_func)
-    rubric.add_reward_func(count_turns_reward_func)
-    rubric.add_reward_func(parser.get_format_reward_func(), weight=0.2)
-
-    vf_env = TextArenaEnv(
-        game="Wordle-v0",
+    return WordleTutorEnv(
         num_train_examples=num_train_examples,
         num_eval_examples=num_eval_examples,
-        system_prompt=system_prompt,
-        parser=parser,
-        rubric=rubric,
-        feedback_fn=wordle_feedback_fn,
+        use_think=use_think,
     )
-    return vf_env
